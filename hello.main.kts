68c665744084d6dc5720570f5f5075e8755768f7
gilesjb <gilesjb@gmail.com>
2025-11-04 16:04:44 -0800

    Simplified directory structure

--- hello.main.kts
+++ hello.main.kts
@@ -3,29 +3,29 @@
 @file:DependsOn("org.copalis:jam:0.9.1")
 
 interface HelloWorld : JavaProject {
-    fun hello() = "Hello World!"
+    fun hello() = "Hello, World!"
 
     fun printHello() {
         println(hello())
     }
 
-    fun helloFile() = read("main/hello.txt")
+    fun helloFile() = read("hello.txt")
 
     fun printFile() {
         println(helloFile())
     }
 
-    fun helloClasses() = javac("classes/main", sourceFiles("main/**.java"))
+    fun helloClasses() = javac("classes", sourceFiles("HelloWorld.java"))
 
     fun runHello() {
         java("-cp", classpath(helloClasses()), "HelloWorld")
     }
 
-    fun printAll() {
+    fun printHellos() {
         printHello()
         printFile()
         runHello()
     }
 }
 
-Project.run(HelloWorld::class.java, HelloWorld::printAll, args)
+Project.run(HelloWorld::class.java, HelloWorld::printHellos, args)
