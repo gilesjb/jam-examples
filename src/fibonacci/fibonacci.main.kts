961799819d32111b863458917ca1f3eb2245b2f4
gilesjb <gilesjb@gmail.com>
2026-02-25 17:52:14 -0800

    Removed repository annotation

--- src/fibonacci/fibonacci.main.kts
+++ src/fibonacci/fibonacci.main.kts
@@ -1,5 +1,5 @@
-#!/usr/bin/env kotlin -Xjvm-default=all
-@file:Repository("https://raw.githubusercontent.com/gilesjb/jam-repo/refs/heads/main")
+#!/usr/bin/env -S kotlin -Xjvm-default=all
+
 @file:DependsOn("org.copalis:jam:0.9.1")
 
 interface Fibonacci : Project {
@@ -7,6 +7,7 @@ interface Fibonacci : Project {
     fun fib(x : Long) : Long = if (x < 2) x else fib(x - 1) + fib(x - 2)
 
     fun fib10() = fib(10)
+    fun fib50() = fib(50)
 }
 
 Project.run(Fibonacci::class.java, Fibonacci::fib10, args)
